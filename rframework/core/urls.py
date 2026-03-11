fb9ef13fccf3803915d1fdb86812b05674f1be3b
tmuzi <tafmuzira@gmail.com>
2026-03-11 11:01:01 +0000

    DRF Quick Start

--- rframework/core/urls.py
+++ rframework/core/urls.py
@@ -16,8 +16,23 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+
+from quickstart import views
+
+# ViewSets can automatically generate URLs for CRUD operations.
+# Just register the ViewSet with a router.
+# NOTE: for more control over the API URLs,
+#       you can simply drop down to regular class-based views,
+#       and write the URL conf yourself.
+
+router = routers.DefaultRouter()
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    # path("admin/", admin.site.urls),
+    path("", include(router.urls)),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework"))
 ]
